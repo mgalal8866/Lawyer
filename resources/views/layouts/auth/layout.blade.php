b38973354324b2ebbbc0cabc7cbd23f819b6f71d
Mohamed Galal <mgalal8866@gmail.com>
2024-06-06 01:05:16 +0300

    modified:   resources/views/layouts/auth/layout.blade.php

--- resources/views/layouts/auth/layout.blade.php
+++ resources/views/layouts/auth/layout.blade.php
@@ -113,7 +113,7 @@
     <!-- BEGIN Vendor JS-->
 
     <!-- BEGIN: Page Vendor JS-->
-    <script src="{{ asset('vendor/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('asset/vendor/jquery.validate.min.js') }}"></script>
     <!-- END: Page Vendor JS-->
 
     <!-- BEGIN: Theme JS-->
